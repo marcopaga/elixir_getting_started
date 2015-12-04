87d534b3ec703847d16866dac4a56e41e8d6c896
Marco Paga <marco.paga@codecentric.de>
2015-12-04 15:11:13 +0100

    list extended

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -1,11 +1,23 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
-    {:ok, bucket} = KV.Bucket.start_link
+  setup do
+    {:ok, bucketPid} = KV.Bucket.start_link
+    {:ok, bucket: bucketPid}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes values by key", testContext do
+    %{bucket: bucketPid} = testContext
+    KV.Bucket.put(bucketPid, "milk", 3)
+    assert KV.Bucket.get(bucketPid, "milk") == 3
+    KV.Bucket.delete(bucketPid, "milk")
+    assert KV.Bucket.get(bucketPid, "milk") == nil
   end
 end
